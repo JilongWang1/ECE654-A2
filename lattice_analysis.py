6333387e8265b6bc12cadf1c74581d47424aa34f
Jilong Wang <j322wang@uwaterloo.ca>
2023-03-25 15:41:31 -0400

    revised display of fix point solving

--- lattice_analysis.py
+++ lattice_analysis.py
@@ -12,6 +12,8 @@ class Lattice:
         self.blocks = {}  # map from block to its index
         self.counting()
         self.lattice = [['B'] * self.var_num for _ in range(len(self.all_block))]
+        self.all_var = '\t' + '\t'.join(sorted(list(self.var.keys()), key = lambda x: self.var[x]))
+        print('For program at', checking_path, ':')
 
     # counting the number of blocks and variables and put them in maps
     def counting(self):
@@ -59,8 +61,23 @@ class Lattice:
 
     # iteratively solving the fixpoint
     def fix_solve(self):
+        step_count = 0
+        print('step 0 :')
+        self.print_lattice()
+
         while self.step():
+            step_count += 1
+            print('step', step_count, ':')
+            self.print_lattice()
             None
+        print('reached a fix point')
+
+    def print_lattice(self):
+        print(self.all_var)
+        for block, line in zip(self.all_block, self.lattice):
+            print(block.at(), '\t', end='')
+            print('\t'.join(line))
+        print()
 
     # one step solving the fixpoint, return if any change is made
     def step(self):
